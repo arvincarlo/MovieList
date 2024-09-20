350a13ac5b6cbe0491188a2cba25798efd93f061
Arvin <arvincris@gmail.com>
2024-09-20 11:32:09 +0800

    Loading movie details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import StarRating from './StarRating';
 
 const tempMovieData = [
   {
@@ -158,7 +159,7 @@ const average = (arr) =>
 const KEY = '8fdeb9af';
 
 export default function App() {
-  const [query, setQuery] = useState("avengers");
+  const [query, setQuery] = useState("walking");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -397,10 +398,67 @@ function WatchedMovie({movie}) {
 }
 
 function MovieDetails({selectedId, onCloseMovie}) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [movie, setMovie] = useState({});
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  console.log(movie);
+  
+
+  useEffect(function() {
+      async function getMovieDetails() {
+        try {
+          setIsLoading(true);
+          const response = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
+          if (!response.ok) throw new Error("Something went wrong with fetching movie details");
+          const data = await response.json();
+          if (data.length <= 0) throw new Error("Something went wrong with fetching movie details");
+          setMovie(data);
+        } catch (error) {
+          console.warn(error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      getMovieDetails();
+    
+  }, [selectedId])
+  
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
-      {selectedId}
+      {
+        isLoading ? <Loader /> : 
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
+            <img src={poster} alt={`Poster of ${movie} movie`}></img>
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>{released} &bull; {runtime}</p>
+              <p>{genre}</p>
+              <p><span>⭐</span>{movie.imdbRating} IMDb rating</p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={24}></StarRating>
+            </div>
+            <p><em>{plot}</em></p>
+            <p>Starring: {actors}</p>
+            <p>Directed by: {director}</p>
+          </section>
+        </>
+      }
     </div>
   ) 
 }
--- src/index.js
+++ src/index.js
@@ -22,11 +22,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} defaultRating={3}/>
-    <StarRating maxRating={10}/>
-    <StarRating maxRating={5} size={23} color="#b4d455"/>
-    <StarRating />
-    <Test />
   </React.StrictMode>
 );
 
